937e21b97943c6ec1030626b930cd1494b2bb81e
firebrandv2 <41483895+firebrandv2@users.noreply.github.com>
2019-04-02 14:30:56 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, './dist/index.html');
+
+// define routes and socket
+const server = express();
+server.get('/', function(req, res) { res.sendFile(INDEX); });
+server.use('/', express.static(path.join(__dirname, './dist/')));
+let requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const io = socketIO(requestHandler);
+
+// Game Server
+import SpaaaceServerEngine from './src/server/SpaaaceServerEngine.js';
+import SpaaaceGameEngine from './src/common/SpaaaceGameEngine.js';
+
+
+// Game Instances
+const gameEngine = new SpaaaceGameEngine({ traceLevel: 1000 });
+const serverEngine = new SpaaaceServerEngine(io, gameEngine, {
+    debug: {},
+    updateRate: 6,
+    timeoutInterval: 0 // no timeout
+});
+
+// start the game
+serverEngine.start();
